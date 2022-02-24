b7c39b8ef53c38f873a6c07589d532caf9ed67d7
stickyfingies <sethtraman@gmail.com>
2022-02-24 12:58:03 -0600

    Add script to .rc files automatically

--- install.sh
+++ install.sh
@@ -7,6 +7,18 @@ function making() { #Makes the script executable
 }
 function moving() { #Moves the script into /usr/bin
 	$(sudo cp shell-random-greeter /usr/bin)
+}
+function config() { #Adds script to shell config
+	if [[ "$SHELL" =~ "bash" ]]; then #if using bash
+		echo "Installing to ~/.bashrc"
+		echo -e "\nshell-random-greeter" >> ~/.bashrc
+		source ~/.bashrc
+	elif [[ "$SHELL" =~ "zsh" ]]; then #if using zsh
+		echo "Installing to ~/.zshrc"
+		echo -e "\nshell-random-greeter" >> ~/.zshrc
+	else
+		echo "Couldn't detect shell.  Please add to shell config manually."
+	fi
 } 
 making 
 if making; then #If making() is successful:
@@ -16,6 +28,7 @@ if making; then #If making() is successful:
 	if moving; then #if moving() is successful:
 		echo "Success!"
 		echo "enter 'shell-random-greeter' into your CLI to make sure it works!"
+		config
 	else #if moving() is unsuccessful
 		echo "ERROR: File was not copied to /usr/bin"
 		echo "Exiting install"
