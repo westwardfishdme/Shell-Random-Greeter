511766f13be01562e9b5a3b0d816c0bc0b72368e
withoutdishonor <94497837+westwardfishdme@users.noreply.github.com>
2022-10-11 09:34:05 -0500

    Added .config files and more
    - creates instant prompt function in .p10k for zsh
    - creates .config files with a linked file for editing /usr/bin/shell-random-greeter without superuser priviledges
    - user-based prompts coming soon!

--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+
+# Simple Install for Shell-random-greeter
+# This file must be in the same directory as the script in order to properly install
+# Essentially this is happening: 
+# 	1. 'shell-random-greeter'File is made an executable
+#	2. shell config file is detected and a line is added invoking the shell.
+#	3. if you would like to only install the command, install manually.
+#	4. create a linked file in .config/shellrandomgreeter to easily append to /usr/bin/shell-random-greeter
+
 echo "Starting Install..."
 echo "Making Script an executable..."
 
 
 #Main Functions
 
-function making() { #Makes the script executable
-	
+function making() { #Makes the script executable	
 	chmod +x $PWD/shell-random-greeter
 }
 
@@ -15,9 +23,10 @@ function moving() { #Moves the script into /usr/bin
 	# if the file is detected in /usr/bin, ask if overwrite is ok.
 	if [[ -f /usr/bin/shell-random-greeter ]]; then
 		echo -e "// there already is a file named 'shell-random-greeter' detected in /usr/bin \n Do you wish to overwrite this file? [y/N]"
-		read yn
+		read -s -n1 yn
 		if [[ $yn == "y" ]] || [[ $yn == "Y" ]]; then #Overwrite
 			echo "Overwriting 'shell-random-greeter' in /usr/bin"
+			sleep 2
 			sudo cp --remove-destination $PWD/shell-random-greeter /usr/bin
 		elif [[ $yn == "n" ]] || [[ $yn == "N" ]]; then #Don't overwrite
 			echo -e "Overwrite cancelled. \n Exiting Script..."
@@ -41,6 +50,7 @@ function detect_tcshrc() { #detect .tcshrc file
 
                 if [[ (($COUNT -lt 1)) ]]; then
                         echo  "appending 'shell-random-greeter' to .tcshrc"
+			sleep 2
                         echo -e "\nshell-random-greeter" >> ~/.tcshrc
                 else
                         echo "Shell Random Greeter is already installed in .tcshrc!"
@@ -50,6 +60,7 @@ function detect_tcshrc() { #detect .tcshrc file
                 touch ~/.tcshrc
                 echo "Done."
                 echo "appending 'shell-random-greeter' to file..."
+		sleep 2
                 echo -e "\nshell-random-greeter" >> ~/.tcshrc
                 echo Done.
         fi
@@ -60,10 +71,21 @@ function detect_zshrc() { #detect .zshrc file
                 echo ".zshrc found!"   
 		if [[ -f ~/.p10k.zsh ]]; then #Detects if user has Power Level 10K
 			echo ".p10k.zsh detected!"
-			COUNT=$(grep -c shell-random-greeter ~/.p10k.zsh)
+			COUNT=$(grep -c shell-random-greeter $HOME/.p10k.zsh)
+			INSTANTPROMPT=$(grep -c instant_prompt_shell-prompt $HOME/.zshrc)
 			if [[ (($COUNT -lt 1))  ]]; then 
-				echo "appending 'shell-random-greeter' to .p10k.zsh"
-				echo -e "\nshell-random-greeter" >> ~/.p10k.zsh
+				echo "creating 'shell-random-greeter' instant prompt to .p10k.zsh"
+				sleep 2
+				echo -e "# shell-random-greeter instant prompt \nfunction prompt_shell_prompt() {\n\tshell-random-greeter\n}" >> ~/.p10k.zsh
+				echo -e "\nfunction instant_prompt_shell_prompt() {\n\tprompt_shell_prompt\n}" >> ~/.p10k.zsh
+				sleep 2
+				echo "invoking instant prompt in ~/.zshrc"
+				sleep 2
+				if [[ $INSTANTPROMPT -lt 1 ]]; then
+					echo -e "instant_prompt_shell_prompt" >> $HOME/.zshrc
+				else
+					echo "Instant Prompt invoked already!"
+				fi
 			else 
 				echo "Shell Random Greeter is already installed in .p10k.zsh!"
 			fi
@@ -72,6 +94,7 @@ function detect_zshrc() { #detect .zshrc file
 
 			if [[ (($COUNT -lt 1)) ]]; then
 				echo "appending 'shell-random-greeter' to .zshrc"
+				sleep 2
 				echo -e "\nshell-random-greeter" >> ~/.zshrc
 			else
 				echo "Shell Random Greeter is already installed in .zshrc!"
@@ -80,8 +103,9 @@ function detect_zshrc() { #detect .zshrc file
 
 	else      
 		echo ".zshrc file not found, making file..."
-		touch ~/.zshrc
+		touch $HOME/.zshrc
 		echo "Done."
+		sleep 2
 		echo "appending 'shell-random-greeter' to file..."
 		echo -e "\nshell-random-greeter" >> ~/.zshrc
 		echo Done.
@@ -95,6 +119,7 @@ function detect_bashrc() { #detect .bashrc file
 
                 if [[ (($COUNT -lt 1)) ]]; then
                         echo "appending 'shell-random-greeter' to .bashrc"
+			sleep 2
                         echo -e "\nshell-random-greeter" >> ~/.bashrc
                 else
                         echo "Shell Random Greeter is already installed in .bashrc!"
@@ -104,6 +129,7 @@ function detect_bashrc() { #detect .bashrc file
                 touch ~/.bashrc
                 echo "Done."
                 echo "appending 'shell-random-greeter' to file..."
+		sleep 2
                 echo -e "\nshell-random-greeter" >> ~/.bashrc
                 echo Done.
         fi
@@ -124,6 +150,7 @@ function detect_kshrc() { #detect .kshrc file
                 touch ~/.kshrc
                 echo "Done."
                 echo "appending 'shell-random-greeter' to file..."
+		sleep 2
                 echo -e "\nshell-random-greeter" >> ~/.kshrc
                 echo Done.
         fi
@@ -178,9 +205,23 @@ if making; then #If making() is successful:
 	echo "Success."
 	echo "Moving File to /usr/bin"
 
-	if  moving && [[ $yn == "y" || $yn == "Y" ]]; then #if moving() is successful:
-		echo "Success!"
-		config
+	if  moving && [[ $yn == "y" ]] || [[ $yn == "Y" ]]; then #if moving() is successful:
+		echo -e "\nSuccess!\n---"
+		echo -e "creating config files..."
+		mkdir $HOME/.config/shellrandomgreeter
+		echo -e "making linked config file..."
+		sudo link /usr/bin/shell-random-greeter $HOME/.config/shellrandomgreeter/config
+		echo "Would you like to add shell-random-greeter to your config file?"
+		read -s -n1 configure
+
+		if [[ $configure == "y" ]] || [[ $configure == "Y" ]]; then
+			config
+		elif [[ $configure == "n" ]] || [[ $configure == "N" ]]; then
+			echo -e "\e[34mshell-random-greeter\e[0m installed!"
+		else
+			echo "unknown input: interpreting as 'n'"
+			echo -e "\e[34mshell-random-greeter\e[0m installed!"
+		fi
 	else #if moving() is unsuccessful
 		echo "ERROR: File was not copied to /usr/bin"
 		echo "Exiting install"
